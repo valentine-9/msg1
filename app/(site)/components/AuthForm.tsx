ce11696d1db28bed64f089ce9757dc95d2685de0
Kevin Nguyen <i42067932@gmail.com>
2023-06-23 19:03:27 -0400

    3

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import Input from '../../components/inputs/Input';
+
+type Variant = 'LOGIN' | 'REGISTER';
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') {
+      setVariant('REGISTER');
+    } else {
+      setVariant('LOGIN');
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === 'REGISTER') {
+      // Axios Register
+    }
+
+    if (variant === 'LOGIN') {
+      // NextAuth SignIn
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    // NextAuth Social Sign In
+  };
+
+  return (
+    <div
+      className='
+      mt-8
+      sm:mx-auto
+      sm:w-full
+      sm:max-w-md
+      '
+    >
+      <div
+        className='
+      bg-white
+      px-4
+      py-8
+      shadow
+      sm:rounded-lg
+      sm:px-10
+      '
+      >
+        <form 
+        className='space-y-6' 
+        onSubmit={handleSubmit(onSubmit)}>
+          <Input 
+            id='email'
+            label='Email' 
+            register={register}
+            errors={errors}/>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
